a8562733084442af502a620ad982a4d939a9c40c
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-08-23 18:40:37 -0500

    removing + in folders for modules

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
 
-import { HomeModule } from './+home/home.module';
+import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
