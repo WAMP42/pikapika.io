afce1a445c3a529db77d72e3ca121a92805abfd8
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-08-23 11:31:51 -0500

    Fonts in production

--- tools/tasks/seed/build.html_css.ts
+++ tools/tasks/seed/build.html_css.ts
@@ -3,7 +3,6 @@ import * as cssnano from 'cssnano';
 import * as gulp from 'gulp';
 import * as gulpLoadPlugins from 'gulp-load-plugins';
 import * as merge from 'merge-stream';
-import * as util from 'gulp-util';
 import { join } from 'path';
 
 import {
@@ -14,9 +13,9 @@ import {
   CSS_SRC,
   DEPENDENCIES,
   ENABLE_SCSS,
-  ENV,
-  TMP_DIR,
   getPluginConfig,
+  ENV,
+  TMP_DIR
 } from '../../config';
 
 const plugins = <any>gulpLoadPlugins();
@@ -28,8 +27,6 @@ const processors = [
     browsers: BROWSER_LIST
   })
 ];
-
-const reportPostCssError = (e: any) => util.log(util.colors.red(e.message));
 
 const isProd = ENV === 'prod';
 
@@ -69,7 +66,6 @@ function processComponentScss() {
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.sass(getPluginConfig('gulp-sass')).on('error', plugins.sass.logError))
     .pipe(plugins.postcss(processors))
-    .on('error', reportPostCssError)
     .pipe(plugins.sourcemaps.write(isProd ? '.' : ''))
     .pipe(gulp.dest(isProd ? TMP_DIR : APP_DEST));
 }
@@ -85,7 +81,6 @@ function processComponentCss() {
   ])
     .pipe(isProd ? plugins.cached('process-component-css') : plugins.util.noop())
     .pipe(plugins.postcss(processors))
-    .on('error', reportPostCssError)
     .pipe(gulp.dest(isProd ? TMP_DIR : APP_DEST));
 }
 
@@ -104,7 +99,6 @@ function processAllExternalStylesheets() {
   return merge(getExternalCssStream(), getExternalScssStream())
     .pipe(isProd ? plugins.concatCss(gulpConcatCssConfig.targetFile, gulpConcatCssConfig.options) : plugins.util.noop())
     .pipe(plugins.postcss(processors))
-    .on('error', reportPostCssError)
     .pipe(isProd ? cleanCss() : plugins.util.noop())
     .pipe(gulp.dest(CSS_DEST));
 }
@@ -149,8 +143,8 @@ function getExternalScss() {
 function processExternalCss() {
   return getExternalCssStream()
     .pipe(plugins.postcss(processors))
+    //.pipe(isProd ? plugins.concatCss(CSS_PROD_BUNDLE) : plugins.util.noop())
     .pipe(isProd ? plugins.concatCss(gulpConcatCssConfig.targetFile, gulpConcatCssConfig.options) : plugins.util.noop())
-    .on('error', reportPostCssError)
     .pipe(isProd ? cleanCss() : plugins.util.noop())
     .pipe(gulp.dest(CSS_DEST));
 }
