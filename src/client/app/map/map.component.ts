a1dc42380b749de656d5196e3dc47dba91e85af1
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-08-26 15:42:47 -0500

    Get current location in map

--- src/client/app/map/map.component.ts
+++ src/client/app/map/map.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  moduleId   : module.id,
-  selector   : 'pika-map',
+  moduleId: module.id,
+  selector: 'pika-map',
   templateUrl: 'map.component.html',
-  styleUrls  : ['map.component.css'],
+  styleUrls: ['map.component.css'],
 })
-export class MapComponent {
-  public lat: number = 51.678418;
-  public lng: number = 7.809007;
+export class MapComponent implements OnInit {
 
+  public lat: number;
+  public lng: number;
+
+  ngOnInit() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (succes) => {
+          let cords = succes.coords;
+          this.lat = cords.latitude;
+          this.lng = cords.longitude;
+        },
+        (err) => {
+          alert('Ups! Something Went Wrong With You Location');
+        }
+      );
+    };
+  }
 }
