ea0f5aefc05fc698bcf270539e49367bb28b18c0
Daniel Ulises Garcia Verdin <dan@hectic.io>
2016-08-26 16:03:38 -0500

    getPokemons from the API

--- src/client/app/map/map.component.ts
+++ src/client/app/map/map.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { APIService } from '../shared/index';
+
 @Component({
-  moduleId: module.id,
-  selector: 'pika-map',
+  moduleId   : module.id,
+  selector   : 'pika-map',
   templateUrl: 'map.component.html',
-  styleUrls: ['map.component.css'],
+  styleUrls  : ['map.component.css'],
 })
 export class MapComponent implements OnInit {
 
   public lat: number;
   public lng: number;
+  public zoom: number = 18;
+
+  constructor(private api: APIService) { }
 
   ngOnInit() {
+    this.getCurrentLocation();
+    this.getPokemon();
+  }
+
+  private getCurrentLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (succes) => {
@@ -24,5 +34,18 @@ export class MapComponent implements OnInit {
         }
       );
     };
+  }
+
+  private getPokemon() {
+    this.api
+        .getNerbyPokemon()
+        .subscribe(
+          (response: any) => {
+            console.log(response);
+          },
+          (error: any) => {
+            alert('Something its wrong with our servers');
+          }
+        );
   }
 }
